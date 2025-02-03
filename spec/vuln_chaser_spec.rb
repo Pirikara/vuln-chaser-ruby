64f3bfeda37f366c15dbbbb1f396fd59b9ce1ea5
Pirikara <65005408+Pirikara@users.noreply.github.com>
2025-02-04 01:53:56 +0900

    add vuln_chaser spec

--- spec/vuln_chaser_spec.rb
+++ spec/vuln_chaser_spec.rb
@@ -1,10 +1,27 @@
+require 'spec_helper'
+
 RSpec.describe VulnChaser do
-    it "has a version number" do
-      expect(VulnChaser::VERSION).not_to be nil
+  describe '.configure' do
+    it 'yields the Config class to the block' do
+      expect { |b| described_class.configure(&b) }.to yield_with_args(VulnChaser::Config)
     end
-  
-    it "does something useful" do
-      expect(false).to eq(true)
+
+    it 'allows configuration through block' do
+      described_class.configure do |config|
+        config.storage_path = '/custom/path'
+        config.custom_paths = ['app/controllers']
+        config.excluded_paths = ['vendor/']
+      end
+
+      expect(VulnChaser::Config.storage_path).to eq('/custom/path')
+      expect(VulnChaser::Config.custom_paths).to eq(['app/controllers'])
+      expect(VulnChaser::Config.excluded_paths).to eq(['vendor/'])
     end
   end
-  
+
+  describe 'VulnChaserTraceError' do
+    it 'is defined as a custom error class' do
+      expect(VulnChaser::VulnChaserTraceError.ancestors).to include(StandardError)
+    end
+  end
+end
