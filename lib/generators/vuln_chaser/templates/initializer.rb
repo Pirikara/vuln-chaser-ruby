3dbb7a2ae7b84dd68a8a71f298c6465392b94a17
Pirikara <65005408+Pirikara@users.noreply.github.com>
2025-02-03 23:12:56 +0900

    remove llm config

--- lib/generators/vuln_chaser/templates/initializer.rb
+++ lib/generators/vuln_chaser/templates/initializer.rb
@@ -1,9 +1,6 @@
 require 'vuln_chaser'
 
 VulnChaser.configure do |config|
-  # OpenAI API Key for LLM analysis
-  config.llm_api_key = ENV['OPENAI_API_KEY']
-
   # Storage path for trace data
   config.storage_path = Rails.root.join('tmp', 'vuln_chaser')
 
